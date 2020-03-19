0776e535b8ac47a138d2a8ec1032891e580e9280
ceccs18c49 <60145964+ceccs18c49@users.noreply.github.com>
2020-03-19 22:58:21 +0530

    Add files via upload

---
+++ 1903/perl/add.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/perl
+
+print "\nEnter two numbers: ";
+
+# read elements
+$a = <>;
+$b = <>;
+
+# compute sum
+$c = $a +$b;
+
+# print sum
+print "\nSum is $c\n"; 
---
+++ 1903/perl/arry.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+# arrays
+@langs = ('c++', 'c', 'java', 'python', 'javascript');
+
+# $lang iterates through each element in @lang
+foreach $lang (@langs) 
+{
+	print "\n$lang";
+}
